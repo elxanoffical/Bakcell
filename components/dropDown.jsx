222f21e04547034a16f5e264cefa4422fc387789
Elxan <elxan.offical.07@gmail.com>
2025-04-19 20:28:36 +0400

    header

---
+++ components/dropDown.jsx
@@ -0,0 +1,62 @@
+// components/Dropdown.jsx
+import Link from "next/link";
+import { IoIosArrowDown } from "react-icons/io";
+
+export default function Dropdown({ title, items, className = "" }) {
+  return (
+    <div className={`relative group inline-block ${className}`}>
+      <div className="flex items-center gap-1 cursor-pointer select-none">
+        <h2
+          className="
+          text-black font-semibold text-[15px]
+          border-b-2 border-transparent
+          group-hover:border-b-black
+        "
+        >
+          {title}
+        </h2>
+        {items ? (
+          <IoIosArrowDown
+            className="
+            w-4 h-4 transform text-black
+            transition-transform duration-200 ease-in-out
+            group-hover:-rotate-180
+          "
+          />
+        ) : (
+          ""
+        )}
+      </div>
+
+      <div
+        className={`
+            ${
+              items
+                ? `absolute left-0 top-full mt-1
+        flex flex-col gap-3 my-1
+        border border-gray-300 bg-white text-black
+        shadow-lg rounded p-2 z-10
+        opacity-0 scale-95 transform
+        pointer-events-none
+        transition-all duration-200 group-hover:duration-500 ease-in-out
+        group-hover:opacity-100 group-hover:scale-100 group-hover:pointer-events-auto`
+                : "hidden"
+            }
+        
+    `}
+      >
+        {items?.map(({ href, label }) => (
+          <Link
+            key={href}
+            href={href}
+            className=" px-3 py-1 font-semibold
+              whitespace-nowrap rounded
+              hover:text-red-600 hover:bg-red-100"
+          >
+            {label}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+}
--- components/header.jsx
+++ components/header.jsx
@@ -3,14 +3,18 @@
 import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { MdOutlineDarkMode } from "react-icons/md";
-import { MdOutlineLightMode } from "react-icons/md";
+import {
+  MdOutlineDarkMode,
+  MdOutlineLightMode,
+  MdSearch,
+  MdApps,
+} from "react-icons/md";
 import balans from "@/app/assets/images/navImg/balans.svg";
 import support from "@/app/assets/images/navImg/support.svg";
+import bakcellLogo from "@/app/assets/images/headerImg/bakcell_logo.svg";
+import Dropdown from "@/components/dropDown";
 
 const Header = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
   const navItems = [
     { href: "#", label: "Fərdi" },
     { href: "#", label: "Biznes" },
@@ -20,8 +24,42 @@ const Header = () => {
 
   const [active, setActive] = useState(0);
 
+  const tarifItems = [
+    {
+      title: "Tariflər",
+      altMenu: [
+        { href: "/star", label: "Star" },
+        { href: "/daimonline", label: "DaimOnline" },
+        { href: "/sade", label: "Sadə" },
+      ],
+    },
+    {
+      title: "Paketlər",
+      altMenu: [
+        { href: "/internet", label: "İnternet üçün" },
+        { href: "/sms", label: "SMS üçün" },
+      ],
+    },
+    {
+      title: "Rouminq",
+      altMenu: [
+        { href: "/paketler", label: "İnternet paketləri" },
+        { href: "/allPaket", label: "Hamısı daxil paketləri" },
+        { href: "/zeng", label: "Zəng paketləri" },
+        { href: "/smspaketleri", label: "SMS paketləri" },
+        { href: "/rouminq", label: "Təyyarədə rouminq" },
+      ],
+    },
+    {
+      title: "xidmetler",
+    },
+    {
+      title: "mobil cihazlar",
+    },
+  ];
+
   return (
-    <section className="fixed w-full shadow-md bg-[#0f0f0f] text-white">
+    <section className="fixed flex flex-col w-full shadow-md bg-[#0f0f0f] text-white">
       <nav className="container mx-auto flex justify-between items-center max-h-[50px] py-[11px]">
         <div className="flex gap-[36px] ">
           {navItems.map((item, x) => (
@@ -42,11 +80,11 @@ const Header = () => {
 
         <div className="flex items-center gap-10">
           <div className="flex gap-1">
-            <Image src={balans} />
+            <Image src={balans} alt="" />
             <h2 className="text-[#f4f2f2] text-[14px] font-medium">Balans</h2>
           </div>
-          <div className="flex">
-            <Image src={support} />
+          <div className="flex gap-1">
+            <Image src={support} alt="" />
             <h2 className="text-[#f4f2f2] text-[14px] font-medium">Dəstək</h2>
           </div>
           <select
@@ -65,12 +103,52 @@ const Header = () => {
             </option>
           </select>
           <div className="flex items-center gap-1 border border-white rounded p-[2px]">
-            <MdOutlineLightMode className="h-[28px] w-[28px] " />
-            <MdOutlineDarkMode className="h-[28px] w-[28px] " />
+            <MdOutlineLightMode className="h-[24px] w-[24px] " />
+            <MdOutlineDarkMode className="h-[24px] w-[24px] " />
           </div>
         </div>
       </nav>
-      <header></header>
+
+      <header className="bg-white border-t border-gray-200">
+        <div className="container mx-auto flex justify-between items-center py-5 px-4">
+          <div className="flex items-center gap-6">
+            <Image src={bakcellLogo} alt="Bakcell Logo" />
+            {tarifItems.map((item, idx) => (
+              <Dropdown key={idx} title={item.title} items={item.altMenu} />
+            ))}
+          </div>
+
+          <div className="flex items-center gap-4 text-[#333]">
+            <button className=" cursor-pointer">
+              <MdSearch className="w-7 h-7" />
+            </button>
+
+            <button className="cursor-pointer">
+              <MdApps className="w-9 h-9" />
+            </button>
+
+            <button
+              className="
+              px-4 py-2  cursor-pointer
+              border border-black 
+              rounded-[8px] 
+              text-[15px] font-semibold
+             
+            "
+            >
+              "Bakcell"-ə qoşul
+            </button>
+
+            <Link
+              href="/login"
+              className="
+                text-[15px] font-semibold cursor-pointer"
+            >
+              Daxıl ol
+            </Link>
+          </div>
+        </div>
+      </header>
     </section>
   );
 };
